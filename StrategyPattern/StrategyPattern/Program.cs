41ddbbf0c5041cfbed88e9c8df26671fdb72cbe1
Tuyen Le <tuyenlnse@gmail.com>
2022-04-01 14:38:35 +0700

    Strategy design pattern

---
+++ StrategyPattern/StrategyPattern/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start getting tickets!");
+
+            var random = new Random();
+
+            for(var i = 0; i < 5; i++)
+            {
+                var ticket = new Ticket();
+                ticket.SetName("Ticket " + (i + 1));
+                ticket.SetPrice(50d * i);
+                GeneratePromoteStrategy(random, ticket);
+                LogTicketDetails(ticket);
+
+                GeneratePromoteStrategy(random, ticket);
+                LogTicketDetails(ticket);
+            }
+        }
+
+        private static void LogTicketDetails(Ticket ticket)
+        {
+            Console.WriteLine(
+                                "Promoted price of " +
+                                ticket.GetName() +
+                                " is " +
+                                ticket.GetPromtedPrice() + " " +
+                                ticket.GetPromoteStrategy().GetType().Name + "\n"
+                                );
+        }
+
+        private static void GeneratePromoteStrategy(Random random, Ticket ticket)
+        {
+            var strategyIndex = random.Next(0, 2);
+
+            switch (strategyIndex)
+            {
+                case 0:
+                    ticket.SetPromoStrategy(new NoDiscountStrategy());
+                    break;
+                case 1:
+                    ticket.SetPromoStrategy(new QuarterDiscountStrategy());
+                    break;
+                default:
+                    ticket.SetPromoStrategy(new HalfDiscountStrategy());
+                    break;
+            }
+        }
+    }
+}
